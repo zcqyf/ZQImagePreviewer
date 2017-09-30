a39e6e29f0c29115be4ca54e626db25ba458a17d
qing <yg@YGdeiMac.local>
2017-09-30 09:46:11 +0800

    first commit

---
+++ ImagePreviewer/ImagePreviewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImagePreviewer
+//
+//  Created by qing on 2017/9/30.
+//  Copyright © 2017年 YG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImagePreviewer/ImagePreviewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImagePreviewer
+//
+//  Created by qing on 2017/9/30.
+//  Copyright © 2017年 YG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
